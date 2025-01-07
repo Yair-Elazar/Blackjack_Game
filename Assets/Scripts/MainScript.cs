f7338a117457c78d929445a8ccc5464a67c3e82f
ShlomoYalo <ShlomoYalo8@gmail.com>
2025-01-07 08:08:06 +0200

    Main, login, sign out pages commit

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Button login;
+    public Button signUp;
+    public Button userLogin;
+    public Button userRegister;
+    public GameObject mainTilte;
+    public GameObject loginUI;
+    public GameObject registerUI;
+
+    void Start()
+    {
+        login.onClick.AddListener(() => LoginClicked());
+        signUp.onClick.AddListener(() => SignUpClicked());
+        userLogin.onClick.AddListener(() => UserLoginClicked());
+       
+    }
+
+    private void UserLoginClicked()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    private void SignUpClicked()
+    {
+        mainTilte.SetActive(false);
+        loginUI.SetActive(false);
+        registerUI.SetActive(true);
+    }
+
+    private void LoginClicked()
+    {
+        mainTilte.SetActive(false);
+        loginUI.SetActive(true);
+        registerUI.SetActive(false);
+    }
+
+    public void backToMenu()
+    {
+        loginUI.SetActive(false);
+        registerUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
